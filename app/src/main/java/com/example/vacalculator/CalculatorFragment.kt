14bc1bb4c4ddea8d619f3c2fd23630eb08366934
Micsha <micshanaydanilov060904@gmail.com>
2022-06-14 14:39:31 +0300

    VacCalculator4

---
+++ app/src/main/java/com/example/vacalculator/CalculatorFragment.kt
@@ -0,0 +1,39 @@
+package com.example.vacalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vacalculator.databinding.FragmentCalculator2Binding
+
+class CalculatorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calculator2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentCalculator2Binding.bind(view)
+
+    }
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_calculator2, container, false)
+//    }
+}
--- app/src/main/java/com/example/vacalculator/MainActivity.kt
+++ app/src/main/java/com/example/vacalculator/MainActivity.kt
@@ -4,12 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.vacalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        changeFragment(CalculatorFragment())
+
+        binding.buttonNavigation.setOnItemSelectedListener{
+
+            when (it.itemId) {
+                R.id.calculator -> {
+                changeFragment(CalculatorFragment())
+                }
+
+                R.id.history -> {
+                changeFragment(FragmentHistory())
+                }
+            }
+            true
+        }
 
 
     }
+
+    fun changeFragment(fmt:Fragment){
+        supportFragmentManager.beginTransaction().replace(binding.fragmentHolder.id, fmt).commit()
+    }
+
 }
